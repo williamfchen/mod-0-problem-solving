63e4e9e4bd2cb3cc20c4bdc8226501da04908859
Will Chen <wfchen1990@gmail.com>
2023-06-02 11:39:56 -0600

    Initial commit

---
+++ challenge3.rb
@@ -0,0 +1,7 @@
+#Write a method or function that removes all instances of the letter <strong>s</strong> in a string. The method or function should accept a string as an argument and return the same string with every instance of the letter <strong>s</strong> removed.
+#create a method to remove the letter "s" from a string.  
+
+def remove_s(word)
+    word.delete("s")
+end
+
